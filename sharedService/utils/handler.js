bfbef2999f91308aab5c5247b4862cc22582a7d0
nishuR31 <nishangithub@gmail.com>
2026-02-11 23:24:00 +0530

    check

--- sharedService/utils/handler.js
+++
@@ -1,9 +0,0 @@
-let handler =
-  (func) =>
-  (...args) =>
-    Promise.resolve(func(...args)).catch((err) => {
-      console.error('Handler Error:', err);
-      throw err;
-    });
-
-export default handler;
